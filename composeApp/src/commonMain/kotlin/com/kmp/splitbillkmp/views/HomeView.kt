9c6cbbb2167e0bd0c32e877383b82ef692883482
juantonguino <juan.tonguino@gmail.com>
2025-12-13 10:39:08 -0500

    more changes

--- composeApp/src/commonMain/kotlin/com/kmp/splitbillkmp/views/HomeView.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/splitbillkmp/views/HomeView.kt
@@ -1,9 +1,12 @@
 package com.kmp.splitbillkmp.views
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowCircleDown
@@ -14,6 +17,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -21,8 +25,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kmp.splitbillkmp.componentes.MainCard
@@ -38,18 +45,27 @@ fun HomeView(){
         CenterAlignedTopAppBar(title = { Text("Top bar", fontWeight = FontWeight.Bold)})
     }){
         padding ->
-        buildHomeViewContent(modifier = Modifier.padding(padding))
+        BuildHomeViewContent(modifier = Modifier.padding(padding))
     }
 }
 
 @Composable
-fun buildHomeViewContent(modifier: Modifier){
+fun BuildHomeViewContent(modifier: Modifier){
     var amount by rememberSaveable { mutableStateOf("") }
     var selectedTip by rememberSaveable { mutableStateOf(10) }
     var numberOfPeople by rememberSaveable { mutableStateOf(1) }
+    var totalTip by rememberSaveable { mutableStateOf(0.0) }
+    var total by rememberSaveable { mutableStateOf(0.0) }
+    var totalByPerson by rememberSaveable { mutableStateOf(0.0) }
+    var focusManager= LocalFocusManager.current
+
     val options = listOf(10, 20, 30)
 
-    Column (modifier= modifier){
+    Column (modifier= modifier.pointerInput( key1 = Unit){
+        detectTapGestures {
+            focusManager.clearFocus()
+        }
+    }){
         MainCard(title = "Hola", description = "Description") {
             OutlinedTextField(
                 label = {Text("Amount")},
@@ -59,9 +75,6 @@ fun buildHomeViewContent(modifier: Modifier){
                 )
             )
             Text(amount)
-            Button({}){
-                Text("Click me!")
-            }
             Row (
                 horizontalArrangement = Arrangement.spacedBy(space = 8.dp)
             ) {
@@ -78,28 +91,55 @@ fun buildHomeViewContent(modifier: Modifier){
                 MainIconButton(
                     imageVector = Icons.Default.ArrowCircleDown,
                     contentDescription = "Down",
-                    onClick = {numberOfPeople=down(numberOfPeople)},
+                    onClick = {numberOfPeople=downV2(numberOfPeople)},
                     modifier = Modifier
                 ){}
                 Text(text = numberOfPeople.toString(), fontSize = 50.sp)
                 MainIconButton(
                     imageVector = Icons.Default.ArrowCircleUp,
-                    contentDescription = "Down",
+                    contentDescription = "Up",
                     onClick = {numberOfPeople++},
                     modifier = Modifier
                 ){}
             }
+            Button(onClick = {
+                val amountDouble = amount.toDoubleOrNull() ?: 0.0
+                val calculateResult = calculateV2(amountDouble, selectedTip, numberOfPeople)
+                totalTip= calculateResult.first
+                total= calculateResult.second
+                totalByPerson= calculateResult.third
+                focusManager.clearFocus()
+            }){
+                Text(text = "Calculate")
+            }
         }
         MainCard(title = "Bill Summary", description = "Description") {
-            MainRow(title = "Tip Ammount", total = 123.0)
-            MainRow(title = "Total", total = 123.0)
+            MainRow(title = "Tip Amount", total = totalTip.roundTowDecimals())
+            MainRow(title = "Total", total = total.roundTowDecimals())
+            Surface (
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp)
+            ){
+                Text(
+                    text = "Each person should pay $${totalByPerson.roundTowDecimals()}",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+            }
         }
     }
 }
 
-fun down(number: Int): Int{
-    if(number>1){
-        return number-1
-    }
-    return number
+fun downV2(number: Int): Int =if (number>1) number-1 else number
+
+fun Double.roundTowDecimals(): Double= kotlin.math.round(this * 100) / 100
+
+fun calculateV2(amount: Double, tip:Int, numberOfPeople:Int): Triple<Double, Double, Double>{
+    val tipRes= amount*(tip.toDouble()/ 100)
+    val totalWithTip= amount+tipRes
+    val totalByPerson= totalWithTip / numberOfPeople
+    return Triple(tipRes, totalWithTip, totalByPerson)
 }
